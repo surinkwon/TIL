e7b3611c005807a5f274993c7677f48c18c3f33d
surinkwon <tnfls7070@gmail.com>
2022-03-12 20:19:48 +0900

    0312

---
+++ python/baekjoon/14503_로봇 청소기.py
@@ -0,0 +1,37 @@
+dr = [-1, 0, 1, 0]
+dc = [0, -1, 0, 1]
+
+total = 0
+N, M = map(int, input().split())
+r, c, d = map(int, input().split())
+room = [list(map(int, input().split())) for _ in range(N)]
+nr = r
+nc = c
+
+if d == 3:
+    d = 1
+elif d == 1:
+    d = 3
+
+
+while True:
+    if room[nr][nc] == 0:
+        r = nr
+        c = nc
+        room[r][c] = 2
+        total += 1
+
+    if room[r][c + 1] and room[r][c - 1] and room[r + 1][c] and room[r - 1][c]:
+        if room[r+dr[(d + 2) % 4]][c+dc[(d + 2) % 4]] == 1:
+            break
+        elif room[r+dr[(d + 2) % 4]][c+dc[(d + 2) % 4]] == 2:
+            r += dr[(d+2) % 4]
+            c += dc[(d + 2) % 4]
+            continue
+
+    d = (d + 1) % 4
+
+    nr = r + dr[d]
+    nc = c + dc[d]
+
+print(total)
