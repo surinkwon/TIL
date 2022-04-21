01aa400df2e69ca6047138b6aa9cf6bf9a8c96d4
surinkwon <tnfls7070@gmail.com>
2022-04-21 10:47:15 +0900

    modified

--- python/baekjoon/9020_골드바흐의 추측.py
+++ python/baekjoon/9020_골드바흐의 추측.py
@@ -18,26 +18,17 @@ for _ in range(T):
     number = int(sys.stdin.readline())
     s = 0
     e = len(prime) - 1
-    pair = []
 
     # 투포인터로 두 수의 합 구하기, 소수를 구했을 때 이미 정렬되어있고, 겹치는 수가 없기 때문에 투포인터 사용 가능
     while s <= e:
         if prime[s] + prime[e] == number:
-            pair.append((prime[s], prime[e]))
+            answer = (prime[s], prime[e])
             s += 1
             e -= 1
         elif prime[s] + prime[e] < number:
             s += 1
         else:
             e -= 1
-    
-    # 구한 결과들 중 차이가 가장 작은 것을 정답으로 저장
-    d = pair[0][1] - pair[0][0]
-    answer = pair[0]
-    for i in range(1, len(pair)):
-        if d > pair[i][1] - pair[i][0]:
-            d = pair[i][1] - pair[i][0]
-            answer = pair[i]
 
     print(answer[0], answer[1])
 
