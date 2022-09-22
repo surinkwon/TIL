e609e41bff27739d04643b9aefc62dc6f3bfc890
surinkwon <tnfls7070@gmail.com>
2022-09-22 23:42:37 +0900

    baekjoon 18352_특정 거리의 도시 찾기

---
+++ python/baekjoon/18352_특정 거리의 도시 찾기.py
@@ -0,0 +1,45 @@
+'''
+다익스트라
+'''
+
+from collections import deque
+
+def findWay(s):
+    q = deque()
+    INF = 99999999
+    v = [INF] * (N + 1)
+    v[s] = 0
+    q.append(s)
+
+    while q:
+        cn = q.popleft()
+
+        for d in range(len(road[cn])):
+            nn = road[cn][d]
+            if v[nn] > v[cn] + 1:
+                q.append(nn)
+                v[nn] = v[cn] + 1
+    
+    return v
+
+
+N, M, K, X = map(int, input().split())
+cnt = 0
+road = [set() for _ in range(N + 1)]
+
+for _ in range(M):
+    s, e = map(int, input().split())
+    road[s].add(e)
+
+for i in range(N + 1):
+    road[i] = list(road[i])
+
+distances = findWay(X)
+
+for i in range(N + 1):
+    if distances[i] == K:
+        cnt += 1
+        print(i)
+
+if not cnt:
+    print(-1)
