ec6f69242e0edacdf22e59a01ee87a3dc960968f
surinkwon <tnfls7070@gmail.com>
2023-04-11 19:43:24 +0900

    [Python] 2023-04-11 baekjoon_2003_수들의 합 2 silver4

--- python/baekjoon/2003_수들의 합 2.py
+++ python/baekjoon/2003_수들의 합 2.py
@@ -13,29 +13,40 @@ if N == 1:
     else:
         print(0)
 else:
-    # 포인터 하나는 리스트의 맨 앞, 하나는 그 옆에 두고 움직여가며 검사
-    p1, p2 = 0, 1
-    total = nums[p1] + nums[p2]
+    # 두 개의 포인터 두고 움직여가며 검사
+    p1 = p2 = 0
+    total = nums[p1]
     cnt = 0
 
-    while p2 < len(nums):
+    while p2 < N:
         # 구간의 합이 목적한 값과 같으면 세어주고 두 포인터 모두 증가
         if total == M:
             cnt += 1
             total -= nums[p1]
             p1 += 1
             p2 += 1
+
             # 인덱스 에러 방지
-            if p2 < len(nums):
+            if p2 < N:
                 total += nums[p2]
+
         # 구간합이 목적한 값보다 작으면 숫자를 하나 더 더해줘야하므로 뒤의 포인터만 증가
         elif total < M:
             p2 += 1
-            if p2 < len(nums):
+
+            if p2 < N:
                 total += nums[p2]
-        # 구간합이 목적한 값보다 크면 숫자를 하나 빼줘야하므로 앞의 포인터만 증가 
+        
+        # 구간합이 목적한 값보다 크면 숫자를 하나 빼줘야하므로 앞의 포인터만 증가
+        # 두 포인터가 같은 지점에 있으면 뒤 포인터도 증가
         else:
             total -= nums[p1]
+            
+            if p1 == p2:
+                p2 += 1
+
+                if p2 < N:
+                    total += nums[p2]
             p1 += 1
 
     print(cnt)
