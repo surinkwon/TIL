e41618fecd3437b775ec840483d1a911fa21d3d8
surinkwon <tnfls7070@gmail.com>
2023-06-05 11:46:45 +0900

    [JS][Array/String] 2023-06-05 Leetcode_88. Merge Sorted Array Easy

---
+++ Javascript/LeetCode/88. Merge Sorted Array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  // merge nums1 and nums2
+  for (let i = m; i < nums1.length; i += 1) {
+      nums1[i] = nums2[i - m]
+  }
+
+  nums1.sort((a, b) => a - b)
+};
