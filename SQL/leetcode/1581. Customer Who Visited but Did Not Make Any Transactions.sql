24caccc5c6d7fd79f2de6cc35b56fa847a39bab4
surinkwon <tnfls7070@gmail.com>
2023-08-17 12:13:52 +0900

    [Refactor] Add another solution

--- SQL/leetcode/1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ SQL/leetcode/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -3,3 +3,12 @@ from Visits left outer join Transactions
 on Visits.visit_id = Transactions.visit_id
 group by customer_id
 having count_no_trans > 0
+
+-- runtime이 빠른 풀이
+/*
+select customer_id, count(*) count_no_trans
+from Visits left outer join Transactions
+on Visits.visit_id = Transactions.visit_id
+where transaction_id is null
+group by customer_id
+*/
