384bb6b8fbfb7abc7583075608c92b716a6a3eae
surinkwon <tnfls7070@gmail.com>
2023-08-22 11:16:12 +0900

    [SQL] 2023-08-22 leetcode_1731. The Number of Employees Which Report to Each Employee Easy GROUP BY, COUNT, ROUND, AVG, JOIN, ORDER BY

---
+++ SQL/leetcode/1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,5 @@
+select t1.employee_id, t1.name, count(*) as reports_count, round(avg(t2.age)) as average_age
+from Employees t1 join Employees t2
+on t1.employee_id = t2.reports_to
+group by t1.employee_id
+order by t1.employee_id
